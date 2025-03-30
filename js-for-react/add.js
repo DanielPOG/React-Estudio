28e339dbd3405b1b2d590320880226735f0b957e
DanielPOG <santiagocaicedo140@gmail.com>
2025-03-30 08:15:33 -0500

    fin-JS-for-react

---
+++ js-for-react/add.js
@@ -0,0 +1,11 @@
+export function add(a,b){
+    return a+b
+}
+export function multiply(a,b){
+    return a*b
+}
+const object={
+    add,
+    multiply
+}
+export default object
--- js-for-react/index.js
+++ js-for-react/index.js
@@ -88,14 +88,120 @@ const objeto2 = () => ({ age:20})
 console.log(objeto2())
 */
 //Return en funciones
+/* 
 const isAutoriazado=true
 const button = document.createElement("button")
 button.innerText="Click me"
 button.addEventListener("click", () =>{
+    
     if(isAutoriazado){
-        alert("Esta autorizado")
-    }else{  
-        alert("No esta autorizado")
-    }
+        return alert("Esta autorizado")
+    }  
+    alert("No esta autorizado")
+    
 })
 document.body.append(button)
+*/
+//Strings literales /interpolacion
+/*
+const backgroundColor="red"
+const color = "white"
+
+const button = document.createElement("button")
+button.innerText="Click me"
+button.style.backgroundColor=` ${backgroundColor} `
+button.style.color=`${color}`
+document.body.append(button)
+const h1 = document.createElement("h1")
+h1.innerText=`Esto es una interpolacion ${backgroundColor} ${color}`
+document.body.append(h1)
+*/
+//arrray methods
+const names = ["pepe","maria","juan","luis"]
+// for (let name = 0; name<names.length; name++){
+//     const element = names[name]
+//     console.log(element)
+// }
+
+// forEach
+//El foreach es un metodo de los arrays que permite recorrer cada elemento del array y ejecutar una funcion por cada uno de ellos
+// names.forEach(name => {
+//     console.log(name)
+// });
+//map crea un nuevo array con los resultados de la funcion que se le pase como argumento
+// const newnames= names.map(name =>{
+//     return name
+// })
+// console.log(newnames)//No modifica el array original, crea un nuevo array con los resultados de la funcion
+//find para encontrar un elemento en el array que cumpla con una condicion
+// const newNames= names.find(name => name === "pepe")
+// console.log(newNames)//pepe
+//filer para filtrar los elementos de un array que cumplan con una condicion
+// const newNames= names.filter(name => name === "pepe" || name ==="luis")//no funciona con && si no se cumple la condicion no devuelve nada
+// console.log(newNames)//devuelve un nuevo array con los elementos que cumplan con la condicion
+//concat para concatenar dos arrays, no modifica el array original
+const newNames= ["paco", "santiago"]
+// console.log(newNames.concat(names))//concatena dos arrays, no modifica el array original
+//spread operator para concatenar dos arrays, modifica el array original
+// const newArray = [...names,...newNames]//concatena dos arrays, modifica el array original
+
+// const user={
+//     name:"daniel",
+//     age:20
+// }
+// const addres={
+//     country:"Peru",
+//     city:"Lima",
+// }
+// const object={
+//     ...user,
+//     ...addres,
+// }
+// console.log(object)
+//EcmaScript Modules
+// import addModule from "./add.js"
+// const { add, multiply } = addModule;
+// console.log(addModule.add(2,3));
+//optional chaining
+// const person={
+//     name:"ryan",
+//     addres:{
+//         city:"Lima"
+//     },
+//     location:{
+
+//     }
+// }
+// console.log(person.location?.city ) //Estp es el operador de encadenamiento opcional, si la propiedad no existe no lanza un error, devuelve undefined
+//async await
+// const ul = document.createElement("ul")
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then (function (response){
+//         //response es un objeto que contiene la respuesta de la peticion
+//         return response.json()
+//     }).then(function (datos){
+//         console.log(datos)
+//         datos.forEach(dato => {
+//             const li = document.createElement("li")
+//             li.innerText = dato.name
+//             ul.append(li)
+//         });
+//         document.body.append(ul)
+//     })
+//     .catch(function (){
+//         console.log("Error al cargar")
+//     })
+
+async function fetchData() {
+    let data = await fetch("https://jsonplaceholder.typicode.com/users");
+    let mostrar = await data.json()
+    mostrar.forEach(element => {
+        const h1 = document.createElement("h1")
+        h1.innerText = element.name
+        document.body.append(h1)
+    });
+    return console.log(mostrar)  ;
+}
+fetchData()
